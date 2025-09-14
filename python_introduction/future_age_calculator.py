326d72d6157ad79121221d060541e3172bcf0d20
lietsahara-2 <oloojeffrey096@gmail.com>
2025-09-14 22:41:02 +0300

    did the remaining

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,4 @@
+print("How old are you?")
+i=30
+age = 27+i
+print(f"In 2050, you will be {age} years old.")
--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -2,4 +2,4 @@ principle=1000
 time=3
 rate=0.05
 interest=principle*time*rate
-print(interest)
+print(f"The simple interest is: {interest}")
