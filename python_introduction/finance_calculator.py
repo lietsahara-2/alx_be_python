c045d56db59570980ffcfa28790f25a46279a8f3
lietsahara-2 <oloojeffrey096@gmail.com>
2025-09-14 23:25:52 +0300

    finance calculator input changed

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,7 +1,5 @@
-print("Enter your monthly income: ")
-i= int(input())
-print("Enter your total monthly expenses: ") 
-x=int(input())
+i= int(input("Enter your monthly income: ")) 
+x=int(input("Enter your total monthly expenses: "))
 monthly_savings=i-x
 projected_savings=monthly_savings*12+(monthly_savings*12*0.05)
 print(f"Your monthly savings are {monthly_savings}")
