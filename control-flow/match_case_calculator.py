edb4c32aff63926fc099bf8fd0aa5aac50d2eeaa
lietsahara-2 <oloojeffrey096@gmail.com>
2025-09-21 20:46:33 +0300

    changed match (n1,n2,operation) to match operation

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -2,15 +2,15 @@ num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 operation = input("Choose the operation (+, -, *, /): ")
 
-match (num1, num2, operation):
-    case (num1, num2, "+"):
+match operation:
+    case "+":
         print(f"The result is {num1 + num2}")
-    case (num1, num2, "-"):
+    case "-":
         print(f"The result is {num1 - num2}")
-    case (num1, num2, "*"):
+    case "*":
         print(f"The result is {num1 * num2}")
-    case (num1, num2, "/"):
-        if num2!=0:
-            print(f"the result is {num1/num2}")
+    case "/":
+        if num2 != 0:
+            print(f"The result is {num1 / num2}")
         else:
             print("Cannot divide by zero")
