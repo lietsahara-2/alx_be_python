2c4ff1d84dd9cd613668efdb2dec3b14537644a2
lietsahara-2 <oloojeffrey096@gmail.com>
2025-09-28 22:20:31 +0300

    changed import datetime to from datetime import date time then delta. classes instead of modules for simpler coding

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,13 +1,14 @@
-import datetime
+from datetime import datetime
+from datetime import timedelta
 
 def display_current_datetime():
     global current_date
-    current_date = datetime.datetime.now()
+    current_date = datetime.now()
     print(f"current date and time:{current_date}")
 
 def calculate_future_date():
     no_of_days = int(input("Enter the number of days to add to the current date: "))
-    no_of_days = datetime.timedelta(days=no_of_days)
+    no_of_days = timedelta(days=no_of_days)
     future_date = current_date + no_of_days
     print(f"Future date: {future_date}")
 
