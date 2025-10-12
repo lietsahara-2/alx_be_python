db068d7942b69256b8d109c01abd0725e787063b
lietsahara-2 <oloojeffrey096@gmail.com>
2025-10-12 18:10:40 +0300

    added spacing between + and * a + b

--- oop/class_static_methods_demo.py
+++ oop/class_static_methods_demo.py
@@ -2,12 +2,12 @@ class Calculator:
    
     @staticmethod
     def add(a, b): #Returns the sum of two numbers. Use the @staticmethod decorator.
-        return a+b
+        return a + b
     
     @classmethod
     def multiply(cls, a, b):# Returns the product of two numbers. Use the @classmethod decorator.
         print(f"Calculation type: {cls.calculation_type}")
-        return a*b
+        return a * b
     #Define a class attribute calculation_type with a value of "Arithmetic Operations" that the multiply class method will reference.
     calculation_type = "Arithmetic Operations"
     def __init__(self, calculation_type):
